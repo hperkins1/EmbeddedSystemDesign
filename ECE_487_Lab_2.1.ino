c9e39c48f337b634baf8e6dae9852e185c182ede
hperkins1 <63885478+hperkins1@users.noreply.github.com>
2021-02-08 23:02:43 -0600

    Add Lab 2 files via upload
    Lab 2 has seven segment display working, Lab 2.1 finishes switch operation and blinking mode

---
+++ ECE_487_Lab_2.1.ino
@@ -0,0 +1,353 @@
+/**************************************************************
+ *  "Introduction to Digital Input/Output"
+ * ECE 487 Lab 2 
+ * 
+ * Author:  Hannah Perkins
+ * CWID:    11472559
+ * Date:    8 February 2021
+ * Course:  ECE 487
+ * 
+ * Function:      Blink onboard and offboard LEDs out of phase
+ *        based on Toggle Switch input. Keep track of decimal
+ *        and hexadecimal count of switch changes on Serial
+ *        Monitor and 7-Segment Display.
+ *  
+ * Description:   Application will blink onboard and offboard 
+ *        LEDs out of phase while Toggle Switch is in "on" 
+ *        position. During Blinking operation, onboard LED will 
+ *        be ON for 1 second and OFF for 1 second. Offboard LED 
+ *        will be exactly out-of-phase: OFF for 1 second and ON 
+ *        for 1 second. LEDs will continuously blink in this mode 
+ *        until Toggle Switch is moved to the "off" position. 
+ *        While Toggle Switch is "off" the LED's will both be off.
+ *        
+ *        The application will keep a count of the total number
+ *        of user inputs (switch changes) that have occurred. 
+ *        This output will be displayed on the Serial Monitor
+ *        in both decimal and hexadecimal. The count range is 
+ *        0 to 15 and will rollover after 15. 
+ *        
+ *        The 7-Segment display will display the hexadecimal 
+ *        value of the count. This will match the count 
+ *        maintained on the Serial Monitor. Note: If decimal 
+ *        point is lit on 7-Segment display, then the number is 
+ *        hexadecimal A through F.
+ *        
+ * Arduino Version: 1.8.14
+ *****************************************************************/
+
+// Name Pins
+#define OnboardLED 13
+#define OffboardLED 9
+#define ToggleSwitch 3
+#define SegA 50
+#define SegB 48
+#define SegC 26
+#define SegD 28
+#define SegE 32
+#define SegF 46
+#define SegG 42
+#define DP 22
+
+void setup() {
+  // Initialize onboard and offboard LED Pins
+  pinMode(OnboardLED, OUTPUT);
+  pinMode(OffboardLED, OUTPUT);
+
+  // Initialize 7-Segment Display Pins
+  pinMode(SegA, OUTPUT);
+  pinMode(SegB, OUTPUT);
+  pinMode(SegC, OUTPUT);
+  pinMode(SegD, OUTPUT);
+  pinMode(SegE, OUTPUT);
+  pinMode(SegF, OUTPUT);
+  pinMode(SegG, OUTPUT);
+  pinMode(DP, OUTPUT);
+
+  // Initialize Toggle Switch input
+  pinMode(ToggleSwitch, INPUT);
+
+  // Set LEDs to OFF
+  digitalWrite(OnboardLED, LOW);
+  digitalWrite(OffboardLED, LOW);
+
+  // Set 7-Segment Display to 0
+  sevenSegDisplay(0);
+
+  // Initialize serial communication (9600 bits/sec)
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // Wait for serial port to connect
+  }
+
+  // Send instructions to User
+  initialPrompt();
+
+  // Print zero to Serial Monitor
+  printCount(0);
+}
+
+void loop() {
+  int count = 0;                                // Initialize count to zero
+  int switchState = digitalRead(ToggleSwitch);  // current switch reading
+  int lastSwitchState = (!switchState);         // set previous reading
+  bool blinkMode = switchState;                 // If switch is ON, LEDs 
+                                                // should begin in blinkMode
+  // Time Variables
+  unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+  unsigned long debounceDelay = 50;    // the debounce time
+                                       // increase if the output flickers
+  // Continuous Loop
+  while(1) {
+    // read the state of the switch into a local variable:
+    int reading = digitalRead(ToggleSwitch);
+    
+    // If the switch changed, due to noise or pressing:
+    if (reading != lastSwitchState) {
+      // reset the debouncing timer
+      lastDebounceTime = millis();
+    }
+
+    // Check if reading has been there longer than debounce delay
+    if ((millis() - lastDebounceTime) > debounceDelay) {
+  
+      // if the button state has changed:
+      if (reading != switchState) {
+        switchState = reading;    
+
+        // Increment count for num of inputs
+        count++;
+
+        // Print count to 7-Seg display and Serial Monitor
+        printCount(count%16);       // count%16 for rollover
+        sevenSegDisplay(count%16);  
+        
+        // Blink LEDs if switch is on
+        if (switchState == HIGH) { 
+          ledsOnMessage(); 
+          blinkMode = HIGH;       // Update blinkMode
+          lastSwitchState = LOW;  // Update switch state
+        } 
+        
+        // Turn off LEDs if switch is off
+        else {
+          ledsOff();
+          blinkMode = LOW;        // Update blinkMode
+          lastSwitchState = HIGH; // Update switch state
+        }
+      }
+    }
+
+    // Check blinkMode and blink LEDs if ON
+    if(blinkMode) { blinkLeds(); }  
+  }
+}
+
+/********************************************************
+ * Function Name:     initialPrompt
+ * Input Parameters:  void
+ * Return Value:      void
+ * Purpose:           Prints instructions to Serial 
+ *                    Monitor and prompts user to toggle
+ *                    switch to blink LEDs
+ *******************************************************/
+void initialPrompt() {
+  Serial.println("\nPlease enter move Toggle Switch to 'on' "
+    "position to begin blinking LEDs. ");
+  Serial.println("To turn off LED's, move "
+    "the Toggle Switch to 'off' position.");
+  Serial.println("\nThe 7-Segment Display and Serial Monitor "
+    "will keep track of the ");
+  Serial.println("total number of user inputs "
+    "that have occurred. (Toggle switch inputs ONLY.)");
+}
+
+/********************************************************
+ * Function Name:     ledsOnMessage
+ * Input Parameters:  void
+ * Return Value:      void
+ * Purpose:           Sends message to Serial Monitor
+ *                    updating user that LEDs have been 
+ *                    turned on
+ *******************************************************/
+void ledsOnMessage() {
+  Serial.println("Turning LEDs ON...");
+  Serial.println("LED blinking Mode: \tON");
+}
+
+/********************************************************
+ * Function Name:     ledsOff
+ * Input Parameters:  void
+ * Return Value:      void
+ * Purpose:           Turns off both onboard and offboard
+ *                    LEDs. Also sends message to Serial
+ *                    Monitor letting user know LEDs 
+ *                    have been turned off
+ *******************************************************/
+void ledsOff() {
+  // Send Message to User via Serial Monitor
+  Serial.println("Turning LEDs OFF...");
+  Serial.println("LED blinking Mode: \tOFF");
+  
+  // Turn off all LEDs on Hardware
+  digitalWrite(OnboardLED, LOW);
+  digitalWrite(OffboardLED, LOW);
+  delay(1000);    // Time delay ~ 1 second
+}
+
+/********************************************************
+ * Function Name:     blinkLeds
+ * Input Parameters:  void
+ * Return Value:      void
+ * Purpose:           Blinks onboard and offboard LEDs
+ *                    out of phase of one another
+ *******************************************************/
+void blinkLeds() {
+  // Onboard LED ON & Offboard LED OFF for 1 second
+  digitalWrite(OnboardLED, HIGH);
+  digitalWrite(OffboardLED, LOW);
+  delay(1000);
+  
+  // Onboard LED OFF & Offboard LED ON for 1 second
+  digitalWrite(OnboardLED, LOW);
+  digitalWrite(OffboardLED, HIGH);
+  delay(1000);
+}
+
+/********************************************************
+ * Function Name:     printCount
+ * Input Parameters:  int count - represents number to be 
+ *                    displayed on Serial Monitor
+ * Return Value:      void
+ * Purpose:           Prints the input count to the Serial 
+ *                    Monitor in both decimal and 
+ *                    hexadecimal values
+ *******************************************************/
+void printCount(int count) {
+  Serial.print("count = (decimal) ");
+  Serial.print(count);
+  Serial.print("\t(hex) ");
+  Serial.print(hex(count));
+  Serial.print("\n");
+}
+
+/********************************************************
+ * Function Name:     hex
+ * Input Parameters:  int count - represents number to be 
+ *                    displayed on Serial Monitor
+ * Return Value:      String
+ * Purpose:           Turns the decimal value of integer
+ *                    into hexadecimal string equivalent
+ *                    ready for printing to Serial Monitor
+ *******************************************************/
+String hex(int count) {
+  if (count < 10) {
+    return String(count);
+  }
+  else if (count == 10) {
+    return "A";
+  }
+  else if (count == 11) {
+    return "B";
+  }
+  else if (count == 12) {
+    return "C";
+  }
+  else if (count == 13) {
+    return "D";
+  }
+  else if (count == 14) {
+    return "E";
+  }
+  else {
+    return "F";
+  }
+}
+
+/********************************************************
+ * Function Name:     sevenSegDisplay
+ * Input Parameters:  int num - represents number to be 
+ *                    displayed on 7-segment display
+ * Return Value:      void
+ * Purpose:           Decodes the integer and calls ssd 
+ *                    function to light up appropriate
+ *                    segments
+ *******************************************************/
+void sevenSegDisplay(int num) {
+  // Range of num should be from 0 to 15
+  switch (num) {
+    case 0:
+      ssd(1,1,1,1,1,1,0,0);
+      break;
+    case 1:
+      ssd(0,1,1,0,0,0,0,0);
+      break;
+    case 2:
+      ssd(1,1,0,1,1,0,1,0);
+      break;
+    case 3:
+      ssd(1,1,1,1,0,0,1,0);
+      break;
+    case 4:
+      ssd(0,1,1,0,0,1,1,0);
+      break;
+    case 5:
+      ssd(1,0,1,1,0,1,1,0);
+      break;
+    case 6:
+      ssd(1,0,1,1,1,1,1,0);
+      break;
+    case 7:
+      ssd(1,1,1,0,0,0,0,0);
+      break;
+    case 8:
+      ssd(1,1,1,1,1,1,1,0);
+      break;
+    case 9:
+      ssd(1,1,1,1,0,1,1,0);
+      break;
+    case 10:
+      ssd(1,1,1,0,1,1,1,1);
+      break;
+    case 11:
+      ssd(0,0,1,1,1,1,1,1);
+      break;
+    case 12:
+      ssd(1,0,0,1,1,1,0,1);
+      break;
+    case 13:
+      ssd(0,1,1,1,1,0,1,1);
+      break;
+    case 14:
+      ssd(1,0,0,1,1,1,1,1);
+      break;
+    case 15:
+      ssd(1,0,0,0,1,1,1,1);
+      break;
+  }
+}
+
+/****************************************************************
+ * Function Name:     ssd
+ * Input Parameters:  bool a - represents segment a of display
+ *                    bool b - segment b
+ *                    bool c - segment c
+ *                    bool d - segment d
+ *                    bool e - segment e
+ *                    bool f - segment f
+ *                    bool g - segment g
+ *                    bool dp - decimal point
+ * Return Value:      void
+ * Purpose:           Lights up 7-Segment display from 
+ *                    0 to F according to input
+ ****************************************************************/
+void ssd(bool a, bool b, bool c, bool d, bool e, bool f, bool g, bool dp) {
+  digitalWrite(SegA, a);
+  digitalWrite(SegB, b);
+  digitalWrite(SegC, c);
+  digitalWrite(SegD, d);
+  digitalWrite(SegE, e);
+  digitalWrite(SegF, f);
+  digitalWrite(SegG, g);
+  digitalWrite(DP, dp);
+}
